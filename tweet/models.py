2d99c3d0fcb8b1c49b5cd8fa2c9a0ae48e527ee0
KooHyunJung <ghj3160@gmail.com>
2022-01-19 18:29:04 +0900

    tweet앱 admin 추가

--- tweet/models.py
+++ tweet/models.py
@@ -1,3 +1,14 @@
+# tweet/models.py
 from django.db import models
+from user.models import UserModel
+
 
 # Create your models here.
+class TweetModel(models.Model):
+    class Meta:
+        db_table = "tweet"
+    # ForeignKey : 내가 다른 정보를 가지고 와서 여기에 넣겠다
+    author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    content = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
