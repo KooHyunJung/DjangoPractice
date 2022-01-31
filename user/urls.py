dd676bb561016357c80a56de318da9348957ab18
KooHyunJung <ghj3160@gmail.com>
2022-01-31 14:11:59 +0900

    password_reset  APP 위치 변경

--- user/urls.py
+++ user/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('sign-in/', views.sign_in_view, name='sign-in'),
     path('logout/', views.logout, name='logout'),
     path('user/', views.user_view, name='user-list'),
-    path('user/follow/<int:id>/', views.user_follow, name='user-follow')
+    path('user/follow/<int:id>/', views.user_follow, name='user-follow'),
+
+    #path('accounts/', include('django.contrib.auth.urls')),
+    path('password_reset/done/', views.PasswordResetDoneView.as_view(template_name='password/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(template_name="password/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', views.PasswordResetCompleteView.as_view(template_name='password/password_reset_complete.html'), name='password_reset_complete'),
 ]
--- user/views.py
+++ user/views.py
@@ -5,6 +5,14 @@ from .models import UserModel
 from django.contrib.auth import get_user_model
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
+from django.contrib.auth.forms import PasswordResetForm
+from django.template.loader import render_to_string
+from django.db.models.query_utils import Q
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
 import re
 
 # 여기는 api 통신을 받고 기능이 실제로 움직이는 곳이다.
@@ -91,3 +99,32 @@ def user_follow(request, id):
     else:
         click_user.followee.add(request.user)
     return redirect('/user')
+
+#비번 리셋
+def password_reset_request(request):
+	if request.method == "POST":
+		password_reset_form = PasswordResetForm(request.POST)
+		if password_reset_form.is_valid():
+			data = password_reset_form.cleaned_data['email']
+			associated_users = UserModel.objects.filter(Q(email=data))
+			if associated_users.exists():
+				for user in associated_users:
+					subject = "Password Reset Requested"
+					email_template_name = "main/password/password_reset_email.txt"
+					c = {
+					"email":user.email,
+					'domain':'127.0.0.1:8000',
+					'site_name': 'Website',
+					"uid": urlsafe_base64_encode(force_bytes(user.pk)),
+					"user": user,
+					'token': default_token_generator.make_token(user),
+					'protocol': 'http',
+					}
+					email = render_to_string(email_template_name, c)
+					try:
+						send_mail(subject, email, 'admin@example.com' , [user.email], fail_silently=False)
+					except BadHeaderError:
+						return HttpResponse('Invalid header found.')
+					return redirect ("/password_reset/done/")
+	password_reset_form = PasswordResetForm()
+	return render(request=request, template_name="password/password_reset.html", context={"password_reset_form":password_reset_form})
