1317ec1f38e1ab0f48be742d0f37cf0f0024c98f
KooHyunJung <ghj3160@gmail.com>
2022-02-02 11:29:12 +0900

    기본 본인인증 #URL 추가, 템플릿 수정

--- user/urls.py
+++ user/urls.py
@@ -20,4 +20,10 @@ urlpatterns = [
     path('password_reset_complete/',
          auth_views.PasswordResetCompleteView.as_view(template_name='password/password_reset_complete.html'),
          name='password_reset_complete'),
+
+    # 장고 기본 URL
+    # path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    # path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
